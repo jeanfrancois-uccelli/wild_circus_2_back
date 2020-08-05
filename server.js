c3b1c015dd7659b57bcd7a4219ca8aa2e56e81df
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-08-05 17:07:37 +0200

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "https://vigorous-snyder-a28d08.netlify.app",
 };
 
 app.use(cors(corsOptions));
