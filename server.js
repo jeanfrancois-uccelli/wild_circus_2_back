dfbe7f30e48fdf02bccc7a33d50496b49013d618
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-08-05 17:00:35 +0200

    try update cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://vigorous-snyder-a28d08.netlify.app",
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
