837266c24e2eb860d51820d8d245ef7c54dc8b19
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-08-05 16:24:28 +0200

    add heroku adress

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "https://wildcircus2back.herokuapp.com/",
 };
 
 app.use(cors(corsOptions));
